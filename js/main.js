176bceb32dc0289b31b1133698c719342274af78
SardorAlijanov <s.alijanov@studentinha.uz>
2020-10-21 08:28:47 +0500

    Added responsibility

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 var mySwiper = new Swiper('.swiper-container', {
-slidesPerView:2,
+slidesPerView:1,
 navigation: {
     nextEl: '.arrow',
+},
+breakpoints:{
+540:{
+    slidesPerView:2
+}
 },
 loop:true,
 
 });
+
+var menuButton=document.querySelector(".menu-button");
+var menu = document.querySelector(".header");
+
+menuButton.addEventListener('click',function(){
+menu.classList.toggle("header-active");
+menuButton.classList.toggle("menu-button-active");
+})
